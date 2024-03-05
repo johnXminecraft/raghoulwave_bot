048d99a0f33e0ff4aa987d930304c7a4d61568f5
rahulinmaksym <rahulinmaksym@gmail.com>
2024-03-05 15:59:40 +0200

    pom.xml

---
+++ raghoulwave/src/main/java/org/raghoul818/raghoulwave/RaghoulwaveApplication.java
@@ -0,0 +1,13 @@
+package org.raghoul818.raghoulwave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaghoulwaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaghoulwaveApplication.class, args);
+    }
+
+}
---
+++ raghoulwave/src/test/java/org/raghoul818/raghoulwave/RaghoulwaveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.raghoul818.raghoulwave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaghoulwaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
